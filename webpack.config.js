dc33cb04dbbfbb816dce49293f77486ee5f70fc7
TheMonist <rogerrosena@gmail.com>
2023-07-05 21:42:57 -0400

    adding packages, webpack, files, and boilerplate html

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
