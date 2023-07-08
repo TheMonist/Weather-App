27579f3572f859f91988fdaebc6a83a603839735
TheMonist <rogerrosena@gmail.com>
2023-07-07 21:57:27 -0400

    adding javascript to handle form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.scss';
+
+//Handles Forms
+const searchBtn = document.querySelector("#searchBtn");
+const searchForm = document.querySelector("#searchForm");
+const searchInput = document.querySelector("#searchInput");
+
+searchForm.addEventListener("submit", 
+    e.preventDefault(), 
+);
+
+searchBtn.addEventListener("click", 
+    e.preventDefault(), 
+    getWeather
+);
+
+//create a function for getting the weather
