6c08f8f5ee070df449bbfdad253fc8798f517931
TheMonist <rogerrosena@gmail.com>
2023-07-15 21:27:13 -0400

    moved header and worked on api calls

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,5 +1,19 @@
 const weather = (() => {
-    async function getWeather(city) {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=fa970c595d3e4d1fb45205849232906&q=${city}&aqi=no`, {mode: 'cors'})
+    const apiKey = 'fa970c595d3e4d1fb45205849232906';
+
+    async function getLocation(city) {
+        try {
+            const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`, {mode:'cors'});
+            const getData = await response.json();
+            return getData; 
+        }
+        catch (error) {
+            alert(error);
+            console.log(error); 
+            return null; 
+        } 
     }
+    return {getLocation}; 
 })();
+
+export default weather;
