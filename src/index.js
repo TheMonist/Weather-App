b91e30ce40cbf13d3a15d43a81e40eed55cc5449
TheMonist <rogerrosena@gmail.com>
2023-07-19 20:57:37 -0400

    fixed bug and centered page

--- src/index.js
+++ src/index.js
@@ -4,44 +4,37 @@ import './styles.scss';
 
 //Handles Forms
 const searchBtn = document.querySelector("#searchBtn");
-const searchForm = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
 const cityName = document.querySelector(".cityName");
 const conditions = document.querySelector(".conditions");
 const temp = document.querySelector(".temp");
 const feelsLike = document.querySelector(".feelsLike");
 
+//Event Listeners
+searchBtn.addEventListener("click", e => {
+    const city = searchInput.value;
+    e.preventDefault();
+    getWeather(city);
+});
+
 //API Function
-async function getWeather(cityValue) {
+async function getWeather(city) {
     const apiKey = 'fa970c595d3e4d1fb45205849232906';
 
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityValue}&aqi=yes`, {mode:'cors'});
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`, {mode:'cors'});
         const getData = await response.json();
-        console.log(response.json())
-        console.log(getData);
         renderWeather(getData)
     }
     catch(error){
         alert('Error: Please Try Again')
     }
-}
+};
 
-//Create a function for getting the weather
+//Create a function for getting the weather 'DOM Functions'
 function renderWeather(getData) {
-    cityName.textContent = getData.location.name; //location.name
-    temp.textContent = getData.current.temp_f; //current.temp_c
-    feelsLike.textContent = getData.current.feelslike_f; //current.feelslike_f
-    conditions.textContent = getData.current.conditions.text; //current.conditions.text
-}
-
-//Event Listeners
-//searchForm.addEventListener("submit", 
-    //e.preventDefault(), 
-//);
-
-searchBtn.addEventListener("click", e => {
-    e.preventDefault();
-    getWeather();
-    renderWeather();
-});
+    cityName.textContent = `${getData.location.name} ${getData.location.region}`; 
+    temp.textContent = `Cuurent Temp: ${getData.current.temp_f}°`; 
+    feelsLike.textContent = `Feels Like: ${getData.current.feelslike_f}°`;
+    conditions.textContent = getData.current.condition.text;
+};
